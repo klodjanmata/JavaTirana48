40786000e7e243fb14eb635e0510dad7c9735309
Klodjan Mata <klodjanmata0@gmail.com>
2025-01-12 15:07:14 +0100

    Book class

---
+++ src/JavaAdvanced/Exercises/Collections/Task2/Genre.java
@@ -0,0 +1,13 @@
+package JavaAdvanced.Exercises.Collections.Task2;
+
+public enum Genre {
+    Historic,
+    Romance,
+    Drame,
+    Novel,
+    Science,
+    Poems,
+    Comic,
+    Biography,
+    Encyclopedia,
+}
