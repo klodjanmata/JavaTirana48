1d76162e426e7e2aae13bdd38ed9bb1a1b7f0b30
Klodjan Mata <klodjanmata0@gmail.com>
2025-02-16 10:23:26 +0100

    Poesy

---
+++ src/JavaAdvanced/Exercises/FileIO/Task3.java
@@ -0,0 +1,56 @@
+package JavaAdvanced.Exercises.FileIO;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task3 {
+    private static final String FILENAME = "Files\\Test.txt";
+    public static void main(String[] args) {
+        List<String> content = readFile();
+        printStringList(content);
+        String toAdd = "\nPoezi nga ChatGPT\n";
+        content.add(6, toAdd);
+        writeFile(content);
+    }
+
+    public static void printStringList(List<String> list) {
+        for(String s : list){
+            System.out.println(s);
+        }
+    }
+
+    public static List<String> readFile() {
+        List<String> list = new ArrayList<>();
+        try(BufferedReader br = new BufferedReader(new FileReader(FILENAME))) {
+            String line;
+            while ((line= br.readLine()) !=null){
+                list.add(line);
+            }
+            br.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public static void writeFile(List<String> list) {
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(FILENAME))){
+            for (String s : list){
+                writer.write(s);
+                writer.newLine();
+            }
+            writer.close();
+        }catch (Exception e){
+            e.printStackTrace();
+
+        }
+
+    }
+
+
+
+}
